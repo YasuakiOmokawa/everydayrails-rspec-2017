8e58102b6323ba2ccd647760cb9548d408689ed6
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2019-06-23 03:02:05 +0000

    chapter 03 finished! But not add exercise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,4 +53,7 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  # allowed cloud9 IP address
+  config.web_console.whitelisted_ips = '159.28.162.112'
 end
